48bf5401432e9efcb23f403ab528a48c6c5969bb
moha23 <mojojojo.reyal@gmail.com>
2020-05-11 14:59:56 +0530

    Update and rename matrixMultRowCol.m to matrix_mult_reg.m

--- MATLAB versions/matrix_mult_reg.m
+++ MATLAB versions/matrix_mult_reg.m
@@ -1,6 +1,6 @@
 %pass matrix a and b to the function to get required matrix c = ab
 
-function c = matrix_vector_mult(a,b)
+function c = matrix_mult_reg(a,b)
 
 [row_a, col_a] = size(a);
 [row_b, col_b] = size(b);
@@ -8,9 +8,9 @@ if col_a ~= row_b
     error('Dimensions not correct for multiplication');
 else
     c = zeros(row_a,col_b);
-    for i = 1:col_b
-        for j = 1:row_a
-            c(j,i) = dot(a(j,:),b(:,i));
+    for i = 1:row_a
+        for j = 1:col_b
+            c(i,j) = dot(a(i,:),b(:,j));
         end
     end
 end
